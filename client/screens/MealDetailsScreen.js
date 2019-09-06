90223721b430de0ae2c48b311e5809b947bab811
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2019-09-05 23:05:00 -0700

    Added a star icon to meal header.

--- client/screens/MealDetailsScreen.js
+++ client/screens/MealDetailsScreen.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import HeaderButton from "../components/HeaderButton";
 
 import { MEALS } from "../data/dummy-data";
 
@@ -22,7 +24,18 @@ MealDetailsScreen.navigationOptions = navigationData => {
   const selectedMeal = MEALS.find(meal => meal.id === mealId);
 
   return {
-    headerTitle: selectedMeal.title
+    headerTitle: selectedMeal.title,
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Favorite"
+          iconName="ios-star"
+          onPress={() => {
+            console.log("Favorite Item!");
+          }}
+        />
+      </HeaderButtons>
+    )
   };
 };
 

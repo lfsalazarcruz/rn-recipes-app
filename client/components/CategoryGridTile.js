8a8af2f5aa051e59b661cd719d134456d8a1b4f9
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2019-09-05 20:27:33 -0700

    Finished basic Categories Screen styles.

---
+++ client/components/CategoryGridTile.js
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+  TouchableOpacity,
+  View,
+  Text,
+  StyleSheet,
+  TouchableNativeFeedback,
+  Platform
+} from "react-native";
+
+const CategoryGridTile = props => {
+  let TouchableComponent = TouchableOpacity;
+
+  if (Platform.OS === "android" && Platform.Version >= 21) {
+    TouchableComponent = TouchableNativeFeedback;
+  }
+  return (
+    <View style={styles.gridItem}>
+      <TouchableComponent onPress={props.onSelect} style={{ flex: 1 }}>
+        <View
+          style={{ ...styles.container, ...{ backgroundColor: props.color } }}
+        >
+          <Text style={styles.text}>{props.title}</Text>
+        </View>
+      </TouchableComponent>
+    </View>
+  );
+};
+
+export default CategoryGridTile;
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
+    overflow: "hidden",
+    borderRadius: 10
+  },
+  container: {
+    flex: 1,
+    borderRadius: 10,
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowRadius: 10,
+    shadowOffset: { width: 0, height: 2 },
+    elevation: 3,
+    padding: 15,
+    justifyContent: "flex-end",
+    alignItems: "flex-end"
+  },
+  text: {
+    fontSize: 22,
+    textAlign: "right"
+  }
+});
--- client/screens/CategoriesScreen.js
+++ client/screens/CategoriesScreen.js
@@ -9,12 +9,15 @@ import {
 } from "react-native";
 
 import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
 
 const CategoriesScreen = props => {
   const renderGridItem = itemData => {
     return (
-      <TouchableOpacity
-        onPress={() => {
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onSelect={() => {
           props.navigation.navigate({
             routeName: "CategoryMeals",
             params: {
@@ -22,12 +25,7 @@ const CategoriesScreen = props => {
             }
           });
         }}
-        style={styles.gridItem}
-      >
-        <View>
-          <Text>{itemData.item.title}</Text>
-        </View>
-      </TouchableOpacity>
+      />
     );
   };
 
@@ -52,10 +50,5 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
-  },
-  gridItem: {
-    flex: 1,
-    margin: 15,
-    height: 150
   }
 });
